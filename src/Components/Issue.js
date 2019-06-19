270595be1720813cb5f8a57820c3c84d475d8bd3
msotnikov <msotnikov@axmor.com>
2019-06-19 16:10:14 +0700

    init project

---
+++ src/Components/Issue.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+
+class Issue extends Component {
+    componentDidMount() {
+
+    }
+
+    render() {
+        return <h1>Issue</h1>
+    }
+}
+
+export default Issue;
---
+++ src/Components/Issues.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+
+class Issues extends Component {
+    componentDidMount() {
+
+    }
+
+    render() {
+        return <h1>Issues</h1>
+    }
+}
+
+export default Issues;
